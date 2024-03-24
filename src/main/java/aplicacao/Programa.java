009b0d8360612950b0765bb644dfb9f3da7f2df0
eduardodea85 <eduardodea@outlook.com>
2024-03-24 02:05:24 -0300

    Adicioando, buscando e removendo pessoa

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,18 +1,48 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+		//Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		//Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		//Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager(); //Conexão com o banco de dados
+		
+		//Código necessário para Salvar as 3 pessoas no banco de dados. precisa trocar o id das pessoas para null.
+		//em.getTransaction().begin(); //quando o jpa não apenas lê o bd, ele precisa de uma transação para iniciar.
+		//em.persist(p1); //Persist é o objeto que salva no banco de dados
+		//em.persist(p2);
+		//em.persist(p3);
+		//em.getTransaction().commit(); //Confirma as alterações feitas no bd.
+		
+		//Buscar uma pessoa do bd, depois de já ter adicionado eles.
+		//Pessoa p = em.find(Pessoa.class, 4);
+		
+		//Para apagar uma pessoa do bd
+		Pessoa p = em.find(Pessoa.class, 4);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+				
+		//System.out.println(p);
+		
+		System.out.println("Pronto");
+		
+		em.close();
+		emf.close(); //fechando a conexão com o bd.
+		
+		//System.out.println(p1);
+		//System.out.println(p2);
+		//System.out.println(p3);
 
 	}
 
--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -3,10 +3,23 @@ package dominio;
 import java.io.Serializable;
 import java.util.Objects;
 
+//import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
 public class Pessoa implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	//@Column(name="nomecompleto")
 	private String nome;
 	private String email;
 	
